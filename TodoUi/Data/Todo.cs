42211978d5f520011c8ac59531d7b99047e17778
uNople <unople@gmail.com>
2021-01-31 12:08:49 +1300

    Implement in-memory TODO store

--- TodoUi/Data/Todo.cs
+++ TodoUi/Data/Todo.cs
@@ -1,9 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TodoUi.Data
 {
     public class Todo
     {
-        
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
     }
 }
--- TodoUi/Data/TodoService.cs
+++ TodoUi/Data/TodoService.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace TodoUi.Data
 {
     public class TodoService
     {
+        public List<Todo> Tasks { get; set; } = new List<Todo>();
         
+        public void AddTask(string title, string description)
+        {
+            Tasks.Add(new Todo
+            {
+                Description = description,
+                Title = title
+            });
+        }
     }
 }
