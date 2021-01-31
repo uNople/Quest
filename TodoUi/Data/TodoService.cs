d1576367c53ab33c35e478561833c3d479ee940f
uNople <unople@gmail.com>
2021-01-31 13:56:09 +1300

    Moved code out of razor file into service

--- TodoUi/Data/TodoService.cs
+++ TodoUi/Data/TodoService.cs
@@ -1,14 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace TodoUi.Data
 {
     public class TodoService
     {
         public List<Todo> Tasks { get; set; } = new List<Todo>();
-        
-        public void AddTask(string title, string description, bool isCompleted)
+        private static Random _rng = new Random();
+
+        public void UpdateHeading(MouseEventArgs e, Todo todo)
+        {
+            Delete(todo);
+        }
+
+        public void AddTask()
+        {
+            AddTask($"Task {_rng.Next()}", $"Description {_rng.Next()}", RandomBool());
+        }
+
+        private bool RandomBool()
+        {
+            return _rng.Next(0, 2) == 0 ? false : true;
+        }
+        private void AddTask(string title, string description, bool isCompleted)
         {
             Tasks.Add(new Todo
             {
@@ -18,7 +34,7 @@ namespace TodoUi.Data
             });
         }
 
-        public void Delete(Todo todo)
+        private void Delete(Todo todo)
         {
             Tasks.Remove(todo);
         }
