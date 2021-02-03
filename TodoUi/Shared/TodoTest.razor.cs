a283f515402debaf8be61f565d9636fe5219516a
uNople <unople@gmail.com>
2021-02-03 20:04:43 +1300

    Moved code into code-behind instead of storing it in the .razor

---
+++ TodoUi/Shared/TodoTest.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoUi.Data;
+
+namespace TodoUi.Shared
+{
+    public partial class TodoTest
+    {
+        [Inject]
+        public TodoService TodoService { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+
+        private void CreateTodo()
+        {
+            TodoService.Create(Title, Description);
+            Title = "";
+            Description = "";
+        }
+    }
+}
