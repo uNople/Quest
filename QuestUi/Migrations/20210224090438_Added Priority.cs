c0ed3a216c511550d86a60bdad607a05d63dfaae
Ilya <ilya@gmail.com>
2021-02-24 22:15:08 +1300

    Add migration for Priority

---
+++ QuestUi/Migrations/20210224090438_Added Priority.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuestUi.Migrations
+{
+    public partial class AddedPriority : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Priority",
+                table: "Quests",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Quests");
+        }
+    }
+}
